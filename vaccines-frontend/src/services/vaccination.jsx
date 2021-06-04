5cdd7c69766d242d13cdf8f55104f73bd83bbad0
villivald <maxim.villivald@gmail.com>
2021-06-04 15:56:59 +0300

    changes from recharts to chartjs

--- vaccines-frontend/src/services/vaccination.jsx
+++ vaccines-frontend/src/services/vaccination.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 const baseUrl = "http://localhost:8000/api/vaccinations";
+// const baseUrl = "https://polar-basin-63646.herokuapp.com/api/vaccinations"; - API on Heroku
 
 const getAll = () => {
   const request = axios.get(baseUrl);
